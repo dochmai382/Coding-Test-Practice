9a32a3bdcb46c2c4cb0b0049296323c49de89312
Dami Cho <dochmai382@gmail.com>
2025-08-22 13:12:31 +0900

    [Bronze V] Title: A+B - 3, Time: 128 ms, Memory: 14244 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int a = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = null;
+        for (int i = 0; i < a; i++) {
+            st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
+            sb.append(n+m).append("\n");
+        }
+        System.out.print(sb);
+    }
+}
