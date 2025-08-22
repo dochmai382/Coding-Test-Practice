2b1e053347ef9f3700e2b1e40019a125139f6bec
Dami Cho <dochmai382@gmail.com>
2025-08-22 12:33:16 +0900

    [Bronze V] Title: 시험 성적, Time: 104 ms, Memory: 14192 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.io.*;
+
+/*
+시험 점수를 입력받아 90 ~ 100점은 A, 
+80 ~ 89점은 B, 
+70 ~ 79점은 C, 
+60 ~ 69점은 D, 
+나머지 점수는 F를 출력하는 프로그램을 작성하시오.
+*/
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int score = Integer.parseInt(st.nextToken());
+        if (score >= 90) {
+            System.out.println("A");
+        } else if (score >= 80) {
+            System.out.println("B");
+        } else if (score >= 70) {
+            System.out.println("C");
+        } else if (score >= 60) {
+            System.out.println("D");
+        } else {
+            System.out.println("F");
+        }  
+    }
+}
