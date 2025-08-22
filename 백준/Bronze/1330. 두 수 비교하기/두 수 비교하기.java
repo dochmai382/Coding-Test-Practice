ea23a4b2711d3319094ba480eb33db4080f1ead7
Dami Cho <dochmai382@gmail.com>
2025-08-22 12:28:14 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 104 ms, Memory: 14148 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+
+/*
+A가 B보다 큰 경우에는 '>'를 출력한다.
+A가 B보다 작은 경우에는 '<'를 출력한다.
+A와 B가 같은 경우에는 '=='를 출력한다.
+*/
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        System.out.println(a > b ? ">" : a < b ? "<" : "==");
+    }
+}
