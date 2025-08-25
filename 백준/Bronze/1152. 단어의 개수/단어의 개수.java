4fb3f338097178ffb16a31e81a22c24e9c32958c
Dami Cho <dochmai382@gmail.com>
2025-08-25 14:19:55 +0900

    [Bronze II] Title: 단어의 개수, Time: 244 ms, Memory: 20200 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
+
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            bw.write(String.valueOf(st.countTokens()));
+        }
+    }
+}
