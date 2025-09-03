131b5f695d0154b399063fc6bb216b5fa9aaf6cb
Dami Cho <dochmai382@gmail.com>
2025-09-03 13:39:11 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 104 ms, Memory: 14128 KB -BaekjoonHub

---
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+         try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+             BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
+
+             int[] dp = new int[11];
+             dp[1] = 1; dp[2] = 2; dp[3] =4;
+             for (int i = 4; i < 11; i++) {
+                 dp[i] = dp[i-3] + dp[i-2] + dp[i-1];
+             }
+             
+             int T = Integer.parseInt(br.readLine());
+
+             StringBuilder sb = new StringBuilder();
+             for (int i = 0; i < T; i++) {
+                 int n = Integer.parseInt(br.readLine());
+                 sb.append(dp[n]).append("\n");
+             }
+
+             bw.write(sb.toString());             
+         }       
+    }
+}
