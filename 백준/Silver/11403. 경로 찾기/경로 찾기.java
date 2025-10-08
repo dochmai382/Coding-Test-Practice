c33b34212ad9baa8f2c05c3a391ed9b169cff816
Dami Cho <dochmai382@gmail.com>
2025-10-08 11:00:33 +0900

    [Silver I] Title: 경로 찾기, Time: 168 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.java
@@ -0,0 +1,43 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        try (
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))
+        ) {
+            int N = Integer.parseInt(br.readLine());
+            int[][] M = new int[N][N];
+            
+            for (int i = 0; i < N; i++) {
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                for (int j = 0; j < N; j++) {
+                    M[i][j] = Integer.parseInt(st.nextToken());
+                }
+            }
+
+            for (int k = 0; k < N; k++) {
+                for (int i = 0; i < N; i++) {
+                    for (int j = 0; j < N; j++) {
+                        if (M[i][k] == 1 && M[k][j] == 1) {
+                            M[i][j] = 1;
+                        }
+                    }
+                }
+            }
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    sb.append(M[i][j]).append(" ");
+                }
+                sb.append("\n");
+            }
+
+            bw.write(sb.toString().trim());
+            
+        }
+    }
+}
