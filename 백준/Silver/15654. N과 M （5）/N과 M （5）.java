13c5b441d4718260946e386d5fdb4c0a28581b16
Dami Cho <dochmai382@gmail.com>
2025-10-13 16:41:59 +0900

    [Silver III] Title: N과 M (5), Time: 296 ms, Memory: 30404 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -0,0 +1,70 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+    private int N;
+    private int M;
+    private int[] arr;
+    private boolean[] visited;
+    private int[] result;
+
+    private StringBuilder sb = new StringBuilder();
+
+    public void input(BufferedReader br) throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        initArray(N, M);
+        
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);        
+    }
+
+    private void initArray(int n, int m) {
+        arr = new int[n];
+        visited = new boolean[n];
+        result = new int[m];
+    }
+
+    private void dfs(int depth) {
+        if (depth == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(result[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        
+        for (int i = 0; i < N; i++) {
+            if (visited[i] == false) {
+                visited[i] = true;
+                result[depth] = arr[i];
+                dfs(depth + 1);
+                
+                visited[i] = false;
+            }
+        }
+    }
+    
+    public String getResult() {
+        dfs(0);
+        return sb.toString().trim();
+    }
+}
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        try (
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))
+        ) {
+            Solution sol = new Solution();
+            sol.input(br);
+            bw.write(sol.getResult());
+        }
+    }
+}
