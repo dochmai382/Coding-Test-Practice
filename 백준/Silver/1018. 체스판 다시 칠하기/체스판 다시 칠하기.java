87701a18cbc65241ca3b21b4e371c226ccb559d0
Dami Cho <dochmai382@gmail.com>
2025-10-14 18:04:17 +0900

    [Silver III] Title: 체스판 다시 칠하기, Time: 104 ms, Memory: 14272 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
@@ -1,57 +1,63 @@
 import java.util.*;
 import java.io.*;
 
-/*
-첫째 줄에 N과 M이 주어진다. N과 M은 8보다 크거나 같고, 50보다 작거나 같은 자연수이다. 
-둘째 줄부터 N개의 줄에는 보드의 각 행의 상태가 주어진다. B는 검은색이며, W는 흰색이다.
+class Solution {
+    private int N;
+    private int M;
+    private char[][] board;
 
-보드가 체스판처럼 칠해져 있다는 보장이 없어서, 지민이는 8×8 크기의 체스판으로 잘라낸 후에 몇 개의 정사각형을 다시 칠해야겠다고 생각했다. 
-당연히 8*8 크기는 아무데서나 골라도 된다. 지민이가 다시 칠해야 하는 정사각형의 최소 개수를 구하는 프로그램을 작성하시오.
-*/
+    public void input(BufferedReader br) throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        board = new char[N][M];
+        for (int i = 0; i < N; i++) {
+            String line = br.readLine();
+            for (int j = 0; j < M; j++) {
+                board[i][j] = line.charAt(j);
+            }
+        }
+    }
+
+    public String solve() {
+        int minCnt = 64;
+
+        for (int r = 0; r <= N-8; r++) {
+            for (int c = 0; c <= M-8; c++) {
+                int countW = 0;
+                int countB = 0;
+
+                for (int i = r; i < r+8; i++) {
+                    for (int j = c; j < c+8; j++) {
+                        if ((i + j) % 2 == 0) {
+                            if (board[i][j] == 'B') countW++;
+                            if (board[i][j] == 'W') countB++;
+                        } else {
+                            if (board[i][j] == 'W') countW++;
+                            if (board[i][j] == 'B') countB++;
+                        }
+                    }
+                }
+
+                int currentMin = Math.min(countW, countB);
+                minCnt = Math.min(minCnt, currentMin);
+            }
+        }
+        
+        return String.valueOf(minCnt);
+    }
+}
 
 class Main {
     public static void main(String[] args) throws IOException {
-        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
-
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int N = Integer.parseInt(st.nextToken());
-            int M = Integer.parseInt(st.nextToken());
-
-            char[][] board = new char[N][M];
-            for (int i = 0; i < N; i++) {
-                String line = br.readLine();
-                for (int j = 0; j < M; j++) {
-                    board[i][j] = line.charAt(j);
-                }
-            }
-
-            int minCnt = 64;
-
-            for (int i = 0; i <= N-8; i++) {
-                for (int j = 0; j <= M-8; j++) {
-                    int cnt = 0;
-
-                    for (int k = i; k < i+8; k++) {
-                        for (int l = j; l < j+8; l++) {
-                            if ((k + l) % 2 == 0) {
-                                if (board[k][l] != 'B') {
-                                    cnt++;
-                                }
-                            } else {
-                                if (board[k][l] != 'W') {
-                                    cnt++;
-                                }
-                            }
-                        }
-                    }
-
-                    cnt = Math.min(cnt, 64 - cnt);
-                    minCnt = Math.min(cnt, minCnt);
-                }
-            }
-
-            bw.write(String.valueOf(minCnt));
+        try (
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))
+        ) {
+            Solution sol = new Solution();
+            sol.input(br);
+            bw.write(sol.solve());
         }
     }
 }
