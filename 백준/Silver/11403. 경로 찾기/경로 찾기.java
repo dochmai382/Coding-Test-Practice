ee663f434f2dfc8a8c410d5c071f424529d7f0f7
Dami Cho <dochmai382@gmail.com>
2025-10-17 20:49:27 +0900

    [Silver I] Title: 경로 찾기, Time: 152 ms, Memory: 15272 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.java
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.java
@@ -1,6 +1,47 @@
 import java.util.*;
-import java.lang.*;
 import java.io.*;
+
+class Solution {
+    private int N;
+    private int[][] G;
+
+    public void input(BufferedReader br) throws IOException {
+        N = Integer.parseInt(br.readLine());
+        G = new int[N][N];
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                G[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+    }
+
+    private void solve() {
+        for (int k = 0; k < N; k++) {
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    if (G[i][k] == 1 && G[k][j] == 1) {
+                        G[i][j] = 1;
+                    }
+                }
+            }
+        }
+    }
+
+    public String getResult() {
+        solve();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                sb.append(G[i][j]).append(" ");
+            }
+            sb.append("\n");
+        }
+
+        return sb.toString().trim();
+    }
+}
 
 class Main {
     public static void main(String[] args) throws IOException {
@@ -8,36 +49,9 @@ class Main {
             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
             BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))
         ) {
-            int N = Integer.parseInt(br.readLine());
-            int[][] M = new int[N][N];
-            
-            for (int i = 0; i < N; i++) {
-                StringTokenizer st = new StringTokenizer(br.readLine());
-                for (int j = 0; j < N; j++) {
-                    M[i][j] = Integer.parseInt(st.nextToken());
-                }
-            }
-
-            for (int k = 0; k < N; k++) {
-                for (int i = 0; i < N; i++) {
-                    for (int j = 0; j < N; j++) {
-                        if (M[i][k] == 1 && M[k][j] == 1) {
-                            M[i][j] = 1;
-                        }
-                    }
-                }
-            }
-
-            StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < N; j++) {
-                    sb.append(M[i][j]).append(" ");
-                }
-                sb.append("\n");
-            }
-
-            bw.write(sb.toString().trim());
-            
+            Solution sol = new Solution();
+            sol.input(br);
+            bw.write(sol.getResult());
         }
     }
 }
